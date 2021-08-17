24979836e42842ed08c8dcd245ea135bf08e64d3
MaryLensin <88140591+MaryLensin@users.noreply.github.com>
2021-08-17 11:20:11 +0300

    Add files via upload

---
+++ DataWrapApp.java
@@ -0,0 +1,53 @@
+// main class
+public class DataWrapApp {
+	public static void main(String[] args) {
+
+		System.out.println(DataWrapper.wrapSquare("array"));
+		System.out.println(DataWrapper.wrapAngle("tag"));
+		System.out.println(DataWrapper.wrapRound(0.5));
+	}
+}
+
+//secondary class
+class DataWrapper {
+	// Round brackets
+	static String wrapRound( String text ) {
+		  return "(" + text + ")";
+		}
+	static String wrapRound( char text ) {
+		  return "(" + text + ")";
+		}
+	static String wrapRound( int text ) {
+		  return "(" + text + ")";
+		}
+	static String wrapRound( double text ) {
+		  return "(" + text + ")";
+		}
+	// Square brackets
+	static String wrapSquare( String text ) {
+		  return "[" + text + "]";
+		}
+	static String wrapSquare( char text ) {
+		  return "[" + text + "]";
+		}
+	static String wrapSquare( int text ) {
+		  return "[" + text + "]";
+		}
+	static String wrapSquare( double text ) {
+		  return "[" + text + "]";
+		}
+	//Angle brackets
+	static String wrapAngle( String text ) {
+		  return "<" + text + ">";
+		}
+	static String wrapAngle( char text ) {
+		  return "<" + text + ">";
+		}
+	static String wrapAngle( int text ) {
+		  return "<" + text + ">";
+		}
+	static String wrapAngle( double text ) {
+		  return "<" + text + ">";
+		}
+
+}
